94c1abad769b028eb50fba8e9e271a6f068c3ba6
djskinner <skinner@destiny-denied.co.uk>
2018-01-03 16:14:33 +0000

    Customised

--- brew.sh
+++ brew.sh
@@ -40,32 +40,11 @@ brew install gnupg
 brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
-brew install screen
-brew install homebrew/php/php56 --with-gmp
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+
 
 # Install other useful binaries.
-brew install ack
-#brew install exiv2
-brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
+
 
 # Remove outdated versions from the cellar.
 brew cleanup
